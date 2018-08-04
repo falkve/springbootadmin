ad8c4dddd2530d5853ff2d69a5d58d516045782d
Anders Falk <anders.falk@leetlab.se>
2018-08-04 14:37:35 +0200

    ..

--- src/main/java/SpringBootAdminApplication.java
+++ src/main/java/SpringBootAdminApplication.java
@@ -1,4 +1,6 @@
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +9,10 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @EnableAdminServer
 public class SpringBootAdminApplication {
+    static final Logger logger = LoggerFactory.getLogger(SpringBootAdminApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAdminApplication.class, args);
+        logger.info("SpringBootAdmin started");
     }
 }
